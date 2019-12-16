5902f2ccf14dd452e743ad0540d7bed0a30102e9
Håkan Fahlstedt <hakan.fahlstedt@kindredgroup.com>
2019-12-16 13:08:37 +0100

    Day 7 part 1

--- day7/part1.js
+++ day7/part1.js
@@ -1,23 +1,53 @@
 const utilities = require('../utilities');
 const IntCodeComputer = require('../intcode-computer');
 
-const opcodes = '3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0';//utilities.loadInput('day7/input.txt');
+const permutate = (arr, size, n, permutations) => {
+    if (size === 1) {
+        permutations.push(arr.slice());
+    }
 
+    for (let i = 0; i < size; i++) {
+        permutate(arr, size-1, n, permutations);
+
+        if (size % 2 === 1) {
+            const first = arr[0];
+            arr[0] = arr[size-1];
+            arr[size-1] = first;
+        } else {
+            const tmp = arr[i];
+            arr[i] = arr[size-1];
+            arr[size-1] = tmp;
+        }
+    }
+
+    return permutations;
+}
+
+const permutateArray = arr => permutate(arr, arr.length, arr.length, []);
+
+const opcodes = utilities.loadInput('day7/input.txt');
 const cpu = new IntCodeComputer();
-
-
-const phaseSettings = [[0,1,2,3,4], [4,3,2,1,0]];
+const phaseSettings = permutateArray([0,1,2,3,4]);
 let lastOutput;
+let maxOutput = 0;
+let optimalSettings;
 
 for (const ps of phaseSettings) {
     lastOutput = 0;
     for (const setting of ps) {
         cpu.loadProgram(opcodes);
-        cpu.inputQueue = [setting, lastOutput];
+        cpu.inputQueue = [lastOutput, setting];
     
-        cpu.run(true);
+        cpu.run(false);
         lastOutput = cpu.out;
     }
-    console.log(lastOutput);
+    //console.log(lastOutput);
+    if (maxOutput < lastOutput) {
+        maxOutput = lastOutput;
+        optimalSettings = ps.slice();
+    }
+
+    maxOutput = Math.max(maxOutput, lastOutput);
 }
 
+console.log(maxOutput, optimalSettings);
